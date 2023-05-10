3f4db598a68ebd125664ef752e8e01f50078e50f
harkusha <illia.harkusha.w@gmail.com>
2023-05-10 19:45:17 +0300

    Minor code cleanup and removal of obvious method comments

--- src/main/java/com/iharkusha/util/menu_mini_framework/Menu.java
+++ src/main/java/com/iharkusha/util/menu_mini_framework/Menu.java
@@ -1,17 +1,12 @@
 package com.iharkusha.util.menu_mini_framework;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * Menu is responsible for creating and displaying a menu, offering a numbered selection 
- * to the user. It handles the user input, validates it and invokes a MenuItem based on 
- * the user selection. Menu takes care of an exit system automatically, so to use it you 
- * just need to add menu items and call the execute() method. 
- */
+
 public class Menu {
 	private static BufferedReader in;
 	public static Menu rootMenu;
@@ -19,14 +14,7 @@ public class Menu {
 	private MenuItem exitItem;
 	private String title;
 	private boolean isRootMenu;
-	
-	/* 
-	 * The default constructor is used to create a new instance of Menu. If it is the first 
-	 * Menu object created it will be assigned the roll of Root Menu. A root menu will be 
-	 * the entry and exit points to the program, and is responsible for creating and closing 
-	 * the input stream. All subsequent menus will be created as non-root or sub menus and
-	 * their exit calls will bring execution back one level in the menu structure.
-	 */
+
 	public Menu() {
 		this.itemList = new ArrayList<MenuItem>();
 		
@@ -44,20 +32,9 @@ public class Menu {
 		}
 		this.exitItem.setExitItem(true); // Let the MenuItem know that it is the exit item for this menu
 	}	
-	
-	/*
-	 * This method adds a MenuItem to the menu. The first item added will be '1' in the
-	 * selection order, the next will be '2' and so on, up until the exit item which will
-	 * be the 'list.size() + 1' as a selection option. 
-	 */
+
 	public void addItem(MenuItem item) { this.itemList.add(item); }
-	
-	/*
-	 * This method executes the menu. It first prints the menu to screen, and then gets a
-	 * MenuItem based on the user selection, before finally invoking the MenuItem. The menu
-	 * is looped over until the user selects the exit item which will break the loop and
-	 * return from 'this' menu.
-	 */
+
 	public void execute() {
 		MenuItem item = null;
 		do {
@@ -67,14 +44,9 @@ public class Menu {
 		}
 		while(!item.isExitItem());
 	}
-	
-	/* Menu uses this to know how to index the exit option. */
+
 	private int getExitIndex() { return this.itemList.size() + 1; }
-	
-	/*
-	 * Menu uses this method to get the user input, validate it and return a MenuItem based 
-	 * on the selection.
-	 */
+
 	private MenuItem getUserInput() {
 		MenuItem item = null;
 		String input = null;
@@ -103,11 +75,7 @@ public class Menu {
 		catch (IOException ex) { ex.printStackTrace(); }
 		finally { return item; }
 	}
-	
-	/*
-	 * Menu uses this method to print the menu with all available options to the
-	 * console.
-	 */
+
 	private void print() {
 		StringBuilder sb = new StringBuilder();
 		
@@ -124,8 +92,7 @@ public class Menu {
 		
 		System.out.print(sb.toString());
 	}
-	
-	/* Use this method to change the menu title. */
+
 	public void setTitle(String title) { this.title = title; }
 	
 	public String toString() {
